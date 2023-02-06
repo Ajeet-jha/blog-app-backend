24add0ed3f9db5cec38872883d637b8c7ee33650
Ajeet-jha <jhaajeetcs@gmail.com>
2023-02-06 20:52:17 +0530

    Added husky for Express app

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 import * as dotenv from 'dotenv';
-dotenv.config();
 
 import express from 'express';
 import cors from 'cors';
-import { dbConnection } from "./config/conn"
+import { dbConnection } from './config/conn';
 
+dotenv.config();
 
 const app = express();
 app.use(cors());
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!!!!')
-})
+	res.send('Hello World!!!!');
+});
+
+app.get('/test', (req, res) => {
+	res.send({ message: 'test page', status: 200 });
+});
 
 app.listen(PORT, () => {
-    dbConnection();
-    console.log(`Example app listening on port ${PORT}`)
-})
+	dbConnection();
+	console.log(`Example app listening on port ${PORT}`);
+});
