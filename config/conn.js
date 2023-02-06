0101620be0a40be9fb2df590bc862fc47db37095
Ajeet-jha <jhaajeetcs@gmail.com>
2023-02-06 20:57:17 +0530

    Added husky for Express app

--- config/conn.js
+++ config/conn.js
@@ -1,25 +1,20 @@
 import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
 dotenv.config();
-import mongoose from "mongoose";
 const {
-    MONGO_DB_USER_NAME,
-    MONGO_DB_USER_PASSWORD,
-    MONGO_DB_CLUSTER,
-    MONGO_DB_NAME
+	MONGO_DB_USER_NAME,
+	MONGO_DB_USER_PASSWORD,
+	MONGO_DB_CLUSTER,
+	MONGO_DB_NAME,
 } = process.env;
-console.log({
-    MONGO_DB_USER_NAME,
-    MONGO_DB_USER_PASSWORD,
-    MONGO_DB_CLUSTER,
-    MONGO_DB_NAME
-});
+
 const URI = `mongodb+srv://${MONGO_DB_USER_NAME}:${MONGO_DB_USER_PASSWORD}@${MONGO_DB_CLUSTER}.xspdxt5.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority`;
 
 export const dbConnection = () => {
-    mongoose.set('strictQuery', true)
-    mongoose
-        .connect(URI, { useNewUrlParser: true })
-        .then(console.log("Mongo connected"))
-        .catch(error => console.log(error.message))
-
-}
+	mongoose.set('strictQuery', true);
+	mongoose
+		.connect(URI, { useNewUrlParser: true })
+		.then(console.log('Mongo connected'))
+		.catch((error) => console.log(error.message));
+};
