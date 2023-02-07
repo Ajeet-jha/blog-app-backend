e55cc3916084febc683b4623b06a20414a15b497
Ajeet-jha <jhaajeetcs@gmail.com>
2023-02-07 11:46:20 +0530

    Added prettier and eslint ignore file.

--- config/conn.js
+++ config/conn.js
@@ -9,12 +9,12 @@ const {
 	MONGO_DB_NAME,
 } = process.env;
 
-const URI = `mongodb+srv://${MONGO_DB_USER_NAME}:${MONGO_DB_USER_PASSWORD}@${MONGO_DB_CLUSTER}.xspdxt5.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority`;
+const URI = `mongodb+srv://${MONGO_DB_USER_NAME}:${MONGO_DB_USER_PASSWORD}@${MONGO_DB_CLUSTER}.xspdxt5.mongodb.net/${MONGO_DB_NAME}`;
 
 export const dbConnection = () => {
 	mongoose.set('strictQuery', true);
 	mongoose
-		.connect(URI, { useNewUrlParser: true })
+		.connect(`${URI}?retryWrites=true&w=majority`, { useNewUrlParser: true })
 		.then(console.log('Mongo connected'))
 		.catch((error) => console.log(error.message));
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    mode: 'development',
-    entry: './server.js',
-    target: 'node',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    }
+	mode: 'development',
+	entry: './server.js',
+	target: 'node',
+	externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
+	externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+		],
+	},
 };
