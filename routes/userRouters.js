2175358ba17c55d811892d59c2e01d74fc81b02f
Ajeet-jha <jhaajeetcs@gmail.com>
2023-02-09 12:32:47 +0530

    Implemented signin controller.

--- routes/userRouters.js
+++ routes/userRouters.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import { upload } from '../middleware/uploadImage';
-import { registerUser, getUsers } from '../controllers/userController';
+import {
+	registerUser,
+	getUsers,
+	signInUser,
+} from '../controllers/userController';
 
 const router = express.Router();
 
 router.post('/register', upload.single('image'), registerUser);
+router.post('/signin', signInUser);
 router.get('/users', getUsers);
 
 export { router };
