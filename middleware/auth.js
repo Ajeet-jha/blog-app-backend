58229fb39af438193d827db88fb417a3db559c7b
Ajeet-jha <jhaajeetcs@gmail.com>
2023-02-09 15:42:38 +0530

    Applied access token and refresh token.

---
+++ middleware/auth.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+
+const { ACCESS_SECRET, REFRESH_SECRECT } = process.env;
+
+export const isAuthenticated = (req, res, next) => {
+	try {
+		let token = req.get('authorization');
+		if (!token) {
+			return res.status(404).json({ success: false, msg: 'Token not found' });
+		}
+		[, token] = token.split(' ');
+		const decoded = jwt.verify(token, ACCESS_SECRET);
+		console.log(decoded);
+		req.email = decoded.email;
+		next();
+	} catch (error) {
+		return res.status(401).json({ success: false, msg: error.message });
+	}
+};
+
+export const verifyRefresh = (email, token) => {
+	try {
+		const decoded = jwt.verify(token, REFRESH_SECRECT);
+		return decoded.email === email;
+	} catch (error) {
+		return false;
+	}
+};
